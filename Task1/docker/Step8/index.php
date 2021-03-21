e6a8d0a6dad9c3c8d42733dbe8b471bb2299702a
sijumary <sijumary20cool@gmail.com>
2021-03-21 10:32:02 +0530

    added docker files

---
+++ Task1/docker/Step8/index.php
@@ -0,0 +1,14 @@
+<?php
+header("content-type: text");
+
+$host = "db";
+$username = "root";
+$pw = "my!!root!!!pw";
+
+$conn = new mysqli($host, $username, $pw);
+
+if($conn->connect_errno > 0) {
+    echo $db->connect_error;
+} else {
+    echo "DB CONNECTION SUCCESSFUL \n\n";
+}
